da785a50e972f6416c0dd82fcc68657371f2b9fe
Cristian Dreyer <cristianbr.dreyer@gmail.com>
2023-03-12 19:59:26 -0300

    logs

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const execFile = util.promisify(require('node:child_process').execFile);
 const videosPath = './videos'
 
 async function record() {
-    const { stdout, stderr } = await execFile(ffmpegStatic, [
+    const { stdout, stderr } = await execFile('ffmpeg', [
         '-f', 'dshow',
         '-i', 'audio="Microphone (G600)"',
         '-f', 'dshow',
@@ -36,32 +36,34 @@ function convert() {
 }
 
 // convert()
+
 record()
     .then(data => console.log(data))
     .catch(err => console.error("ERROR:", err.message))
+console.log("start recording")
+console.log("press CTRL+C to stop")
+// // capture screen accord to dimensions
+// 'ffmpeg -f gdigrab -video_size 1920x1080 -i desktop -framerate 30 test.avi'
 
-// capture screen accord to dimensions
-'ffmpeg -f gdigrab -video_size 1920x1080 -i desktop -framerate 30 test.avi'
+// // capture screen with mic (testing)
+// 'ffmpeg -f dshow -i audio="Microphone (G600)" -f gdigrab -video_size 1920x1080 -i desktop -framerate 30 test.avi'
 
-// capture screen with mic (testing)
-'ffmpeg -f dshow -i audio="Microphone (G600)" -f gdigrab -video_size 1920x1080 -i desktop -framerate 30 test.avi'
+// // list devices
+// 'ffmpeg -list_devices true -f dshow -i dummy'
 
-// list devices
-'ffmpeg -list_devices true -f dshow -i dummy'
+// // record webcam
+// 'ffmpeg -f dshow -i video="V380 FHD Camera" cam.avi'
 
-// record webcam
-'ffmpeg -f dshow -i video="V380 FHD Camera" cam.avi'
+// // web and system sound
+// 'ffmpeg -f dshow -i video="V380 FHD Camera":audio="Mixagem estéreo (Realtek High Definition Audio)" cam.avi'
 
-// web and system sound
-'ffmpeg -f dshow -i video="V380 FHD Camera":audio="Mixagem estéreo (Realtek High Definition Audio)" cam.avi'
+// // web and micro and system sound
+// 'ffmpeg -f dshow -i video="V380 FHD Camera":audio="Mixagem estéreo (Realtek High Definition Audio)" -f dshow -i audio="Microphone (G600)" cam.avi'
 
-// web and micro and system sound
-'ffmpeg -f dshow -i video="V380 FHD Camera":audio="Mixagem estéreo (Realtek High Definition Audio)" -f dshow -i audio="Microphone (G600)" cam.avi'
+// // desktop | micro | system sound
+// 'ffmpeg -f dshow -i audio="Microphone (G600)" -f dshow -i audio="Mixagem estéreo (Realtek High Definition Audio)" -f gdigrab -video_size 1920x1080 -i desktop -framerate 30 -filter_complex "[0:a][1:a]amerge=inputs=2[a]" -map 2 -map "[a]" screen.avi'
 
-// desktop | micro | system sound
-'ffmpeg -f dshow -i audio="Microphone (G600)" -f dshow -i audio="Mixagem estéreo (Realtek High Definition Audio)" -f gdigrab -video_size 1920x1080 -i desktop -framerate 30 -filter_complex "[0:a][1:a]amerge=inputs=2[a]" -map 2 -map "[a]" screen.avi'
+// // TESTS
+// 'ffmpeg -f dshow -i audio="Microphone (G600)" -f dshow -i audio="Mixagem estéreo (Realtek High Definition Audio)" -f gdigrab -video_size 1920x1080 -i desktop -framerate 30 -filter_complex "[0:a][1:a]amerge=inputs=2[a]" -map 2 -map "[a]" screen.avi'
 
-// TESTS
-'ffmpeg -f dshow -i audio="Microphone (G600)" -f dshow -i audio="Mixagem estéreo (Realtek High Definition Audio)" -f gdigrab -video_size 1920x1080 -i desktop -framerate 30 -filter_complex "[0:a][1:a]amerge=inputs=2[a]" -map 2 -map "[a]" screen.avi'
-
-'ffmpeg -f dshow -i audio="Microphone (G600)" -f dshow -i audio="Mixagem estéreo (Realtek High Definition Audio)" -f gdigrab -video_size 1920x1080 -i desktop -framerate 30 -filter_complex "[0:a][1:a]amerge=inputs=2[a]" -map 2 -map "[a]" testaaa.avi'
+// 'ffmpeg -f dshow -i audio="Microphone (G600)" -f dshow -i audio="Mixagem estéreo (Realtek High Definition Audio)" -f gdigrab -video_size 1920x1080 -i desktop -framerate 30 -filter_complex "[0:a][1:a]amerge=inputs=2[a]" -map 2 -map "[a]" testaaa.avi'
